ee771214d5394f8152fb1b21291f591b68fb4f24
CindyTham <cindytham@hotmail.com>
2020-09-28 16:14:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print ('Motor Bike Costs £2000')
+bike = 2000 
+print ('Every year it loses 10% of its bike value')
+value = 2000
+while value > 1000:
+ print('£',value)
+ value = value * 0.9
+else:
+     print('Value will now go below £1000')
+
+
+
+
+
+
+
